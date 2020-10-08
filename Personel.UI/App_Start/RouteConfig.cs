a4e34631911ee3f0a530cb284567d535b35f08fd
22BK <bugrakirman@outlook.com>
2020-10-08 16:08:13 +0300

    EF DB First and bootstrap added

--- Personel.UI/App_Start/RouteConfig.cs
+++ Personel.UI/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Personel.UI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Departman", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
