8e49c7a1432ad46f74ba36f52e9e7b0e4d7e24c5
22BK <bugrakirman@outlook.com>
2020-10-09 17:44:17 +0300

    display data annotation added

--- Personel.UI/Models/EntityFramework/Departman.cs
+++ Personel.UI/Models/EntityFramework/Departman.cs
@@ -11,7 +11,8 @@ namespace Personel.UI.Models.EntityFramework
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Departman
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,8 +22,9 @@ namespace Personel.UI.Models.EntityFramework
         }
     
         public int Id { get; set; }
+        [Display(Name ="Departman Adý")]
         public string Ad { get; set; }
-    
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Personel> Personel { get; set; }
     }
