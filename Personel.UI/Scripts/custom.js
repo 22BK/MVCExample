b01d3cab76e6435e3173936560356c722ee3631b
22BK <bugrakirman@outlook.com>
2020-10-17 22:44:36 +0300

    delete by using ajax and confirm box with bootbox nuget package

--- Personel.UI/Scripts/custom.js
+++ Personel.UI/Scripts/custom.js
@@ -1,4 +1,23 @@
-﻿function CheckDateTypeIsValid(dateElement) {
+﻿$(function () {
+    $("#tblDepartmanlar").on("click", ".btnDepartmanSil", function () {
+        var btn = $(this);
+        bootbox.confirm("Departmanı silmek istediğinizden emin misiniz ?", function (result) {
+            if (result) {
+                var id = btn.data("id");
+                $.ajax({
+                    type: "GET",
+                    url: "/Departman/Sil/" + id,
+                    success: function () {
+                        btn.parent().parent().remove();
+                        console.log("silme işlemi başarılı");
+                    }
+                });
+            }
+        })
+    });
+});
+
+function CheckDateTypeIsValid(dateElement) {
     var value = $(dateElement).val();
     if (value=='') {
         $(dateElement).valid("false");
